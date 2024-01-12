3e875fe62f6fa8f51321003975cb83fd75cefe30
OvenMagnetron <jessekramer15@gmail.com>
2024-01-12 09:17:42 +0100

    Schets van JS

---
+++ main.js
@@ -0,0 +1,23 @@
+// Kleine schets hoe ik denk dat de JS er ongeveer uit moet gaan zien.
+
+var magicMode = false
+
+function checkMagic() {
+    if (magicMode == false) {
+
+    } else if (magicMode == true) {
+
+    } else {
+        console.log("Error: Magicmode is", magicMode)
+    }
+}
+
+function toggleMagic() {
+    if (buttonispressed) {
+        var magicMode = true;
+        checkMagic;
+    } else {
+        var magicMode = false;
+        checkMagic;
+    }
+}
