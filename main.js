8a3990897fae2f4d80733a2ee183913b69bdaebb
OvenMagnetron <jessekramer15@gmail.com>
2024-01-16 10:09:54 +0100

    Werkende functie

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 // Kleine schets hoe ik denk dat de JS er ongeveer uit moet gaan zien.
 
-var magicMode = false
+let magicMode = false;
 
 function checkMagic() {
+    var defaultElements = document.querySelectorAll(".default");
+    var magicElements = document.querySelectorAll(".magic");
+
     if (magicMode == false) {
+        magicElements.forEach(function(element) {
+            element.style.display = 'none';
+        });
+        defaultElements.forEach(function(element) {
+            element.style.display = 'block';
+        });
 
+        magicMode = true;
     } else if (magicMode == true) {
+        magicElements.forEach(function(element) {
+            element.style.display = 'block';
+        });
+        defaultElements.forEach(function(element) {
+            element.style.display = 'none';
+        });
 
+        magicMode = false;
     } else {
-        console.log("Error: Magicmode is", magicMode)
+        console.log("Error: Magicmode is", magicMode);
     }
 }
 
-function toggleMagic() {
-    if (buttonispressed) {
-        var magicMode = true;
-        checkMagic;
-
-    } else {
-        var magicMode = false;
-        checkMagic;
-    }
-}
+window.onload = function() {
+    checkMagic();
+};
