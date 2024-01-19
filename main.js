e669b56baaae061884e5852505baaf6e145cb2cd
OvenMagnetron <jessekramer15@gmail.com>
2024-01-19 10:41:46 +0100

    Comments + mensen

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-
 // Magic mode
 let magicMode = false;
 
 function checkMagic() {
+    // Selecteer de standaard en magische elementen
     var defaultElements = document.querySelectorAll(".default");
     var magicElements = document.querySelectorAll(".magic");
 
+    // Controleer of magicMode uit staat
     if (magicMode == false) {
+        // Verberg magische elementen en toon standaard elementen
         magicElements.forEach(function (element) {
             element.style.display = 'none';
         });
@@ -14,8 +16,12 @@ function checkMagic() {
             element.style.display = 'block';
         });
 
+        // Zet magicMode aan
         magicMode = true;
-    } else if (magicMode == true) {
+    }
+    // Controleer of magicMode aan staat
+    else if (magicMode == true) {
+        // Toon magische elementen en verberg standaard elementen
         magicElements.forEach(function (element) {
             element.style.display = 'block';
         });
@@ -23,38 +29,48 @@ function checkMagic() {
             element.style.display = 'none';
         });
 
+        // Zet magicMode uit
         magicMode = false;
-    } else {
+    }
+    // Onverwachte situatie
+    else {
         console.log("Error: Magicmode is", magicMode);
     }
 }
 
-// // Filters
+// Filters
 function filterQ(e) {
+    // Selecteer alle details elementen
     const Qmensen = document.querySelectorAll("details");
+    // Haal het filter op uit het event
     let filter = e.target.dataset.filter;
+    // Controleer het filter
     if (filter === '*') {
+        // Toon alle personen
         Qmensen.forEach(person => person.classList.remove('hidden'));
     } else {
+        // Toon of verberg personen op basis van het filter
         Qmensen.forEach(person => {
             person.classList.contains(filter) ?
                 person.classList.remove('hidden') :
                 person.classList.add('hidden');
         });
-    };
-};
-
+    }
+}
 
 // Window onload
 window.onload = function () {
+    // Voer checkMagic uit bij het laden van de pagina
     checkMagic();
 
+    // Selecteer alle details elementen
     var detailsElements = document.querySelectorAll("details");
 
+    // Voeg event listeners toe aan details elementen op basis van schermgrootte
     detailsElements.forEach(details => {
-        if (screen.width <= 960) { // Check of de grootte groter is dan 960pixels (mobiel)
+        if (screen.width <= 960) { // Controleer of schermgrootte kleiner is dan 960 pixels (mobiel)
             console.log("Scherm onder 960px, Scherm grootte is" + screen.width)
-            details.addEventListener("click", function () { // Mobiel
+            details.addEventListener("click", function () { // Voor mobiel
                 if (details.hasAttribute('open')) {
                     details.removeAttribute('open');
                     console.log("Verwijder open")
